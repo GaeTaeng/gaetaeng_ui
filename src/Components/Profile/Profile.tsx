659473adf8e922d26741a277d9e1c52fa8e62c73
Kim TaeHun <nuckly60@gmail.com>
2023-08-13 15:25:09 +0900

    색상 컬러 지정 및 profile 구조 수정작업

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -4,20 +4,41 @@ import './Profile.css'
 export default function Profile() {
     return (
         <div className='profile'>
-            <hr />
-            <div className='white-background section right'>
+            
+            <div className='white-background section text-right'>
                 Who is GaeTaeng?
             </div>
-            <div className='dark-background section left'>
-            My Skills
+            <div className='dark-background section text-left'>
+                My Skills
+            
             </div>
 
-            <div className='white-background section right'>
+            <div className='dark-background section text-right'>
             My Project
             </div>
 
-            <div className='dark-background section left'>
+            <div className='white-background section text-left'>
             Gym & BodyProfile
+            
+            </div>
+
+            <br />
+            <br />
+            <br />
+            <br />
+
+            
+            <div className='white-background section text-right'>
+                Who is GaeTaeng?
+            </div>
+            <div className='dark-background section text-left'>
+            My Skills
+            </div>
+
+            <div className='dark-background section text-right'>
+            </div>
+
+            <div className='white-background section text-left'>
             </div>
 
 
